a0e40b9e3c23e302f7c26a5752205166f3ee1e2a
Pantagruel74 <tostar74@mail.ru>
2023-03-14 05:27:14 +0500

    Finish solution task1 on clojurescript

--- test 1-4/clojurescript/src/main/yandex/yandex1.cljs
+++ test 1-4/clojurescript/src/main/yandex/yandex1.cljs
@@ -2,8 +2,8 @@
 
 (defn counter? [x]
   (and (coll? x)
-       (not (empty? x.el))
-       (not (empty? x.c))))
+       (contains? x :el)
+       (contains? x :c)))
 
 (defn count-same [el arr]
   {:pre [(int? el) (coll? arr)]}
@@ -11,9 +11,14 @@
 
 (defn count-min-in-2-arrs [el arr1 arr2]
   {:pre [(int? el) (coll? arr1) (coll? arr2)]}
-  [:el el :c (min (count-same el arr1) (count-same el arr2))])
+  {:el el :c (min (count-same el arr1) (count-same el arr2))})
+
+(defn counters-to-arr [v-counters]
+  {:pre [(coll? v-counters) (every? #(counter? %) v-counters)]}
+  (mapv #(vec (repeat (get % :c) (get % :el))) v-counters))
 
 (defn yandex1 [arr1 arr2]
   { :pre [(coll? arr1) (coll? arr2)] }
-  (map #(count-min-in-2-arrs % arr1 arr2) 
-       (distinct (filter #(some (fn [x] (= % x)) arr2) arr1))))
+  (reduce concat
+          (counters-to-arr (map #(count-min-in-2-arrs % arr1 arr2)
+                                (distinct (filter #(some (fn [x] (= % x)) arr2) arr1))))))
