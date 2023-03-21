7918280ca8364543c1a794b7d8c8bb53d72f04a0
Pantagruel74 <tostar74@mail.ru>
2023-03-22 01:51:06 +0500

    add activation function to test 7

--- test 7/neural/src/neural/node.cljc
+++ test 7/neural/src/neural/node.cljc
@@ -1,5 +1,7 @@
 (ns neural.node)
 
+(defn exp [] 2.7183)
+
 (defn arr-of-nums? [x] (and (coll? x) (every? number? x)))
 
 (defn summate 
@@ -7,3 +9,9 @@
   [c-vals]
   {:pre [(arr-of-nums? c-vals)]}
   (reduce + 0 c-vals))
+
+(defn activ-func
+  "Применяет функцию активации к значению"
+  [n-val]
+  {:pre [(number? n-val)]}
+  (/ 1 (+ 1 (Math/pow (exp) (* -1 n-val)))))
--- test 7/neural/test/neural/node_test.clj
+++ test 7/neural/test/neural/node_test.clj
@@ -5,3 +5,19 @@
 (deftest test-summate
   (testing "summate"
     (is (= (summate [0.7 3 2.5 4]) 10.2))))
+
+(deftest test-exp
+  (testing "exp"
+    (is (= 2.7183 (exp)))))
+
+(deftest test-activ-func1
+  (testing "activ-func1"
+    (is (< (abs (- 1 (activ-func 10))) 0.1))))
+
+(deftest test-activ-func2
+  (testing "activ-func2"
+    (is (< (abs (- 0 (activ-func -10))) 0.1))))
+
+(deftest test-activ-func3
+  (testing "activ-func3"
+    (is (< (abs (- 0.5 (activ-func 0))) 0.1))))
