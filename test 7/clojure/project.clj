e870b1c287b1d9122eea6bad72efa8c2f9f2a704
Pantagruel74 <tostar74@mail.ru>
2023-03-22 05:44:48 +0500

    try compile clojure to project

--- test 7/clojure/project.clj
+++ test 7/clojure/project.clj
@@ -6,5 +6,4 @@
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot neural.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar {:aot :all}})
--- test 7/clojure/src/neural/core.clj
+++ test 7/clojure/src/neural/core.clj
@@ -2,12 +2,13 @@
   (:gen-class)
   (:require [neural.node :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
 (defn test-performance []
   (println (System/currentTimeMillis)
            (reduce + 0 (map #(activ-func %) (vec (range 1000000))))
            (System/currentTimeMillis)))
+           
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (test-performance))
+
