cad35b5d950fc18e2b1606fd50214b3e8229cc26
Pantagruel74 <tostar74@mail.ru>
2023-04-09 04:50:28 +0500

    add advanced test for haxe

--- test 7/haxe/hx/src/Checker.hx
+++ test 7/haxe/hx/src/Checker.hx
@@ -1,17 +1,20 @@
 package hx.src;
 
 import Math;
+import Random;
 import hx.src.ArcFunc;
 
 class Checker
 {
     public static function main() {
-        var range = [for (i in 1...1000000) i];
+        var range = [for (i in 1...1000000) i]
+            .map( it -> Random.float(-5, 5) );
         var startDate = Date.now();
         var result = range.map(it -> ArcFunc.calc(it));
         var finishDate = Date.now();
         var msDelta = finishDate.getTime() - startDate.getTime();
         Sys.println("Time delta: " + msDelta + " ms");
         Sys.println("Result len " + result.length);
+        Sys.println("Result sum " + Lambda.fold(result, (i, acc) -> acc + i, 0));
     }
 }
