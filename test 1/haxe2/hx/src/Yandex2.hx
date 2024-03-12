412d4acd398ff7d7c1ee780bd70f4140885e7bbb
Pantagruel74 <tostar74@mail.ru>
2024-03-13 00:17:30 +0500

    add formatter for haxe

--- test 1/haxe2/hx/src/Yandex2.hx
+++ test 1/haxe2/hx/src/Yandex2.hx
@@ -3,29 +3,32 @@ package hx.src;
 using Lambda;
 
 typedef Counter = {
-	var ch:String;
-	var cnt:Int;
+	var ch : String;
+	var cnt : Int;
 }
 
 class Yandex2 {
-	public static function encode(str:String):String {
-		var chars = str.split("");
-		var counters = chars.fold((el, acc:Array<Counter>) -> {
-			if (acc.length == 0) {
-				return [{ch: el, cnt: 1,}];
+
+	public static function encode( str : String ) : String {
+		var chars = str.split( "" );
+		var counters = chars.fold( ( el, acc : Array<Counter> ) -> {
+			if ( acc.length == 0 ) {
+				return [{ ch : el, cnt : 1, }];
 			}
 			var last = acc[acc.length - 1];
-			if (last.ch == el) {
-				return acc.slice(0, acc.length - 1).concat([{ch: el, cnt: last.cnt + 1}]);
+			if ( last.ch == el ) {
+				return acc
+					.slice( 0, acc.length - 1 )
+					.concat( [{ ch : el, cnt : last.cnt + 1 }] );
 			}
-			return acc.concat([{ch: el, cnt: 1}]);
-		}, []);
-		return counters.fold((el, acc:String) -> {
-			return acc + (el.cnt == 1 ? el.ch : (el.ch + Std.string(el.cnt)));
-		}, "");
+			return acc.concat( [{ ch : el, cnt : 1 }] );
+		}, [] );
+		return counters.fold( ( el, acc : String ) -> {
+			return acc + ( el.cnt == 1 ? el.ch : ( el.ch + Std.string( el.cnt ) ) );
+		}, "" );
 	}
 
 	public static function main() {
-		Sys.println(Yandex2.encode("AAABBBCCXYZDDDDAAAABB"));
+		Sys.println( Yandex2.encode( "AAABBBCCXYZDDDDAAAABB" ) );
 	}
 }
--- test 1/haxe2/hx/src/Yandex3.hx
+++ test 1/haxe2/hx/src/Yandex3.hx
@@ -3,26 +3,28 @@ package hx.src;
 using Lambda;
 
 typedef Grouper = {
-	var strs:Array<String>;
-	var id:String;
+	var strs : Array<String>;
+	var id : String;
 }
 
 class Yandex3 {
-	public static function group(arr:Array<String>):Array<Array<String>> {
-		var grouped:Array<Grouper> = arr.fold((el, acc:Array<Grouper>) -> {
-			var id1 = el.split("");
-			id1.sort((a, b) -> ((a > b) ? 1 : -1));
-			var id = id1.fold((el, acc:String) -> (acc + el), "");
-			var groupIndex = acc.findIndex(g -> g.id == id);
-			if (groupIndex == -1) {
-				return acc.concat([{strs: [el], id: id}]);
+
+	public static function group( arr : Array<String> ) : Array<Array<String>> {
+		var grouped : Array<Grouper> = arr.fold( ( el, acc : Array<Grouper> ) -> {
+			var id1 = el.split( "" );
+			id1.sort( ( a, b ) -> (( a > b ) ? 1 : -1 ) );
+			var id = id1.fold( ( el, acc : String ) -> (acc + el ), "" );
+			var groupIndex = acc.findIndex( g -> g.id == id );
+			if ( groupIndex == -1 ) {
+				return acc.concat( [{ strs : [el], id : id }] );
 			}
-			return acc.mapi((i, v) -> (i == groupIndex) ? v : {id: v.id, strs: v.strs.concat([el])});
-		}, []);
-		return grouped.map(el -> el.strs);
+			return acc.mapi( ( i, v ) ->
+				(i == groupIndex ) ? v : { id : v.id, strs : v.strs.concat( [el] ) } );
+		}, [] );
+		return grouped.map( el -> el.strs );
 	}
 
 	public static function main() {
-		Sys.println(Yandex3.group(["eat", "tea", "tan", "ate", "nat", "bat"]));
+		Sys.println( Yandex3.group( ["eat", "tea", "tan", "ate", "nat", "bat"] ) );
 	}
 }
